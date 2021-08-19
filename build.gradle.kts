bd3819f738f3521ed91d2e6377b63822e17a4dfa
TheRealJan <jan.m.tennert@gmail.com>
2021-08-19 21:18:27 +0200

    Fix jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("maven-publish")
 }
 
 group = "de.jan.translatable"
@@ -9,6 +10,18 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = group.toString()
+            artifactId = "Translatable"
+            version = version
+
+            from(components["kotlin"])
+        }
+    }
 }
 
 tasks.test {
